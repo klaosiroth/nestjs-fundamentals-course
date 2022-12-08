79cf945bb1b8462138b25e6239127df00784dd06
Natthaphon Wichianphong <natthaphon@tyche.co.th>
2022-12-08 11:48:22 +0700

    Handling update and delete requests

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,19 +1,18 @@
 import {
   Body,
   Controller,
+  Delete,
   Get,
-  HttpCode,
-  HttpStatus,
   Param,
+  Patch,
   Post,
-  Res,
 } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
   @Get()
-  findAll(@Res() response) {
-    response.status(200).send('This action returns all coffees');
+  findAll() {
+    return 'This action returns all coffees';
   }
 
   @Get(':id')
@@ -22,9 +21,18 @@ export class CoffeesController {
   }
 
   @Post()
-  @HttpCode(HttpStatus.GONE)
   create(@Body() body) {
     return body;
     // return `This action creates a coffee`;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body) {
+    return `This action updates #${id} coffee`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `This action removes #${id} coffee`;
   }
 }
