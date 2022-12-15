b5b57b49cabd4a9b6af91e0ada1edeaf0da75a5b
Natthaphon Wichianphong <natthaphon@tyche.co.th>
2022-12-15 16:06:11 +0700

    Creating a TypeORM Entity

--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -1,6 +1,17 @@
+/* eslint-disable prettier/prettier */
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity() // sql table === 'coffee'
 export class Coffee {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
   name: string;
+
+  @Column()
   brand: string;
+
+  @Column('json', { nullable: true })
   flavors: string[];
 }
