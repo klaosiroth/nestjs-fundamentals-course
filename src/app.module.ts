830ae2740226622cfcafdec8685bcec9f6a4b8b5
Natthaphon Wichianphong <natthaphon@tyche.co.th>
2022-12-16 17:56:11 +0700

    Custom Configuration Files

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,12 @@ import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
 import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
 import { ConfigModule } from '@nestjs/config';
+import appConfig from './config/app.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
+      load: [appConfig],
       validationSchema: Joi.object({
         DATABASE_HOST: Joi.required(),
         DATABASE_PORT: Joi.number().default(5432),
